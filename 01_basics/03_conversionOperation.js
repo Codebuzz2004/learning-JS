6e7b8e51e771577ec82f5488ba9dffd6fc29db8b
Codebuzz2004 <anupammondal6262@gmail.com>
2025-12-29 09:45:14 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,5 +49,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 42
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************* OPERATIONS *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(3/2);
+// console.log(3%2);
+
+let str1 = "Hello"
+let str2 = " Raj"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not a good code
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+//  read incremental operator postfix and prefix
