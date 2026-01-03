67350e9118fca5dcc97cdde463348aa2f1e42128
Codebuzz2004 <anupammondal6262@gmail.com>
2026-01-03 15:19:36 +0000

    date and time

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0 , 23 , 5, 30 , 45)
+let myCreatedDate = new Date("2023-01-23")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now()
+
+// console.log(myTimestamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));// time in seconds
+
+let newDate = new Date()
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+
+// `${newDate.getDate()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday : 'long',
+    year : 'numeric',
+    month : 'long',
+    })
