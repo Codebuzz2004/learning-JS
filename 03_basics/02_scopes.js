28b24691c1f97e81f9d6ae1aacf7f3bed2946060
Codebuzz2004 <anupammondal6262@gmail.com>
2026-01-07 18:14:05 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,44 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log('INNER: ', a);
+    // console.log('INNER: ', a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Anupam"
+
+    function two(){
+        const website = "anupam.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if (true){
+    const username = "Anupam"
+    if(username === "Anupam"){
+        const website = " youtube"
+        console.log(username + website); 
+    }
+    // console.log(website);  
+}
+// console.log(username);
+
+// ++++++++++++++++++++interesting +++++++++++++++++++
+
+addOne(5) // works because of hoisting
+function addOne(num){
+    return num + 1
+}
+
+addTwo(3) // error: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
